41a2b1da6d6e48fcccbb6bccdb50afb1d1e6f57e
Sam <samerinwilliams@yahoo.com>
2018-03-13 11:27:16 -0700

    Finish lesson for To Do Project

--- ToDoProject/assets/js/todos.js
+++ ToDoProject/assets/js/todos.js
@@ -1,12 +1,14 @@
 $("ul").on("click", "li", markDone);
-$("ul").on("click", "li span", deleteToDo);
+$("ul").on("click", "span", deleteToDo);
 $("input[type='text']").keypress(addToDo);
+$("#toggle-form").click(toggleForm);
 
 function markDone(event) {
 	$(this).toggleClass("done");
 	event.stopPropagation();
 }
 function deleteToDo(event) {
+	console.log("deleting");
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
@@ -15,7 +17,19 @@ function deleteToDo(event) {
 
 function addToDo(event) {
 	if (event.which === 13) {
-		$("ul").append("<li><span>X</span> " + $(this).val() + "</li>");
+		$("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + $(this).val() + "</li>");
 		$(this).val("");
 	}
+}
+
+function toggleForm() {
+	$("input[type='text']").fadeToggle();
+	if ($("h1 span").class === "rotate") {
+		$("h1 span").addClass("unrotate");
+		$("h1 span").removeClass("rotate");
+	} else {
+		$("h1 span").removeClass("unrotate");
+		$("h1 span").addClass("rotate");
+	}
+
 }
