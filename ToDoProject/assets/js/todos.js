45ff6b9cb413ea9e130d93c8c059d417504d3a62
Sam <samerinwilliams@yahoo.com>
2018-03-14 11:14:36 -0700

    Polish To Do Project
    Expandable background, basic long to-do support, done list

--- ToDoProject/assets/js/todos.js
+++ ToDoProject/assets/js/todos.js
@@ -2,34 +2,61 @@ $("ul").on("click", "li", markDone);
 $("ul").on("click", "span", deleteToDo);
 $("input[type='text']").keypress(addToDo);
 $("#toggle-form").click(toggleForm);
+$(".delete-all").click(deleteAllToDo);
 
 function markDone(event) {
 	$(this).toggleClass("done");
+	if ($(this).parent().is("#main-list")) {
+		$("#done-list").append("<li class='done'><span><i class='far fa-trash-alt'></i></span> " + $(this).text() + "</li>");
+		$(this).fadeOut(500);
+		$(this).remove();
+	} else {
+		$("#main-list").append("<li><span><i class='far fa-trash-alt'></i></span> " + $(this).text() + "</li>");
+		$(this).fadeOut(500);
+		$(this).remove();
+	}
 	event.stopPropagation();
 }
+
 function deleteToDo(event) {
-	console.log("deleting");
-	$(this).parent().fadeOut(500, function(){
+	$(this).parent().fadeOut(500, function() {
 		$(this).remove();
 	});
 	event.stopPropagation();
 }
 
+function deleteAllToDo(event) {
+	$(this).parent().siblings().find("li").fadeOut(500, function() {
+		$(this).remove();
+	});
+
+	// animate trash can icon
+	// $(this).find("i").animate([
+	// 		//keyframes
+	// 		{dataFaTransform: "shrink-8"},
+	// 		{dataFaTransform: "grow-8"}
+	// 	], 500);
+	event.stopPropagation();
+}
+
 function addToDo(event) {
 	if (event.which === 13) {
-		$("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + $(this).val() + "</li>");
+		$("#main-list").append("<li><span><i class='far fa-trash-alt'></i></span> " + $(this).val() + "</li>");
 		$(this).val("");
 	}
 }
 
 function toggleForm() {
 	$("input[type='text']").fadeToggle();
-	if ($("h1 span").class === "rotate") {
-		$("h1 span").addClass("unrotate");
-		$("h1 span").removeClass("rotate");
-	} else {
-		$("h1 span").removeClass("unrotate");
-		$("h1 span").addClass("rotate");
-	}
+	toggleIcon($(this));
+}
 
+function toggleIcon(elem) {
+	if (elem.hasClass("open")) {
+		elem.addClass("closed");
+		elem.removeClass("open");
+	} else {
+		elem.removeClass("closed");
+		elem.addClass("open");
+	}
 }
